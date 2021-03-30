13c01b09be5f02c6e7223d8687dddad8a38619ad
Louis-wan <louis10508@gmail.com>
2021-03-29 20:59:31 -0700

    add US

--- driving.py
+++ driving.py
@@ -1,8 +1,13 @@
-county = input('where are you come frome')
+country = input('where are you come frome ')
 age = input('how old are you')
 age = int (age)
 if country == '台灣':
 	if age >= 18: 
 		print('你可以考駕照')
 	else: 
-		print ('not')
+		print('not')
+elif country == 'American':
+	if age >= 16: 
+		print('你可以考駕照')
+	else: 
+		print('not')	
